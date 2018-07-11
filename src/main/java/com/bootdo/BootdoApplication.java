307eaf93321d9195ec11c0e1353c13f75e007b25
兰继明 <uffxoje2007@qq.com>
2018-07-11 23:52:18 +0800

    bootdo项目的demo项目，第一次提交

---
+++ src/main/java/com/bootdo/BootdoApplication.java
@@ -0,0 +1,31 @@
+package com.bootdo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@ServletComponentScan
+//@MapperScan(basePackages={"com.bootdo.**.mapper"})
+//@MapperScan(basePackages={"com.bootdo.**.mapper","com.ruoyi.**.mapper"})
+@MapperScan(basePackages={"com.bootdo.**.dao","com.ruoyi.**.mapper"})
+@SpringBootApplication
+@EnableCaching
+@ComponentScan(basePackages={"com.bootdo","com.ruoyi"})
+public class BootdoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootdoApplication.class, args);
+        System.out.println("ヾ(◍°∇°◍)ﾉﾞ    bootdo启动成功      ヾ(◍°∇°◍)ﾉﾞ\n" +
+                " ______                    _   ______            \n" +
+                "|_   _ \\                  / |_|_   _ `.          \n" +
+                "  | |_) |   .--.    .--. `| |-' | | `. \\  .--.   \n" +
+                "  |  __'. / .'`\\ \\/ .'`\\ \\| |   | |  | |/ .'`\\ \\ \n" +
+                " _| |__) || \\__. || \\__. || |, _| |_.' /| \\__. | \n" +
+                "|_______/  '.__.'  '.__.' \\__/|______.'  '.__.'  ");
+    }
+}
